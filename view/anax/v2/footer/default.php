6d3d3a0f9e41c3d0cdead8f4fd835f6a5651910e
8ptk4 <putte.karlsson@gmail.com>
2018-09-05 11:58:51 +0100

    Changed footer style and content

--- view/anax/v2/footer/default.php
+++ view/anax/v2/footer/default.php
@@ -9,6 +9,9 @@ namespace Anax\View;
 // Show incoming variables and view helper functions
 //echo showEnvironment(get_defined_vars(), get_defined_functions());
 
-?>
-
-FOOTER
+?><p>Sidan gjord utav Patrik Karlsson för kursen oophp</p>
+<div class="footer_links">
+    <a href="http://validator.w3.org/check/referer">HTML5</a>
+    <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>
+    <a href="http://validator.w3.org/unicorn/check?ucn_uri=referer&;ucn_task=conformance">Unicorn</a>
+</div>
